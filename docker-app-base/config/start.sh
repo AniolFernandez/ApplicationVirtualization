10bee61fd1bb22564ca56526cd3771466371bec1
AniolFernandez <aniol.fdz@gmail.com>
2023-03-06 19:57:38 +0100

    fix script permission

--- docker-app-base/config/start.sh
+++ docker-app-base/config/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export DISPLAY=:99
-/usr/bin/Xvfb $DISPLAY -screen 0 1280x720x24+32 -ac -nolisten tcp -nolisten unix &
+/usr/bin/Xvfb $DISPLAY -screen 0 1920x1080x24+32 -ac -nolisten tcp -nolisten unix &
 x11vnc -display $DISPLAY -listen localhost -forever &
 /usr/share/novnc/utils/launch.sh --vnc localhost:5900 &
 i3 &
