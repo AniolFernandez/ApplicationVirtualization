d2e3b781fa7c080e9704282e5b9a8ef815a1f9ea
AniolFernandez <aniol.fdz@gmail.com>
2023-04-03 19:10:53 +0200

    feat: dev db connection

--- web-server/server/db.js
+++ web-server/server/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST || '192.168.56.101',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || 'root',
+    database: process.env.DB_DATABASE || 'appvirt',
 });
 
 connection.connect((error) => {
--- web-server/server/index.js
+++ web-server/server/index.js
@@ -4,10 +4,6 @@ const usersRouter = require('./controller/user');
 
 
 const port = process.env.PORT || 3000;
-const DB_HOST = process.env.DB_HOST || '127.0.0.1';
-const DB_USER = process.env.DB_USER || 'root';
-const DB_PASSWORD = process.env.DB_PASSWORD || 'root';
-const DB_DATABASE = process.env.DB_DATABASE || 'appvirt';
 
 const app = express();
 
