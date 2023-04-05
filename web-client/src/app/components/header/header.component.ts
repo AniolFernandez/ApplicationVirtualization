fd525694314f03696336d41bf8793f3210204a9c
AniolFernandez <aniol.fdz@gmail.com>
2023-04-05 19:23:03 +0200

    fix: redirecció al premer el logo

--- web-client/src/app/components/header/header.component.ts
+++ web-client/src/app/components/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { State } from 'src/app/State';
 import jwt_decode from 'jwt-decode';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -9,16 +10,21 @@ import jwt_decode from 'jwt-decode';
 })
 export class HeaderComponent {
 
-  constructor(){
+  constructor(private router: Router) {
     let token = localStorage.getItem('token');
-    if(token){
-      this.username = jwt_decode<{user:string}>(token)["user"];
+    if (token) {
+      this.username = jwt_decode<{ user: string }>(token)["user"];
     }
   }
 
-  public username: string ="";
+  public username: string = "";
 
-  toggleAside(){
+  toggleAside() {
     State.asideOpen = !State.asideOpen;
+  }
+
+  //Redirect a la plana d'apps
+  main() {
+    this.router.navigate(['/']);
   }
 }
