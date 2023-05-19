29e94cd20522f71b4be028327cb8b2c4829b5b0a
AniolFernandez <aniol.fdz@gmail.com>
2023-05-19 22:47:00 +0200

    feat: fix default pass

--- install/inidb.sh
+++ install/inidb.sh
@@ -5,7 +5,7 @@ sudo apt-get install mysql-client-core-8.0 -y > /dev/null
 read -p "Escriu el host on es situa la BDD: [Per defecte: 127.0.0.1]: " MYSQL_HOST 
 MYSQL_HOST=${MYSQL_HOST:-"127.0.0.1"} 
 read -p "Escriu la cotrasenya de root [Per defecte: root]: " MYSQL_PASSWORD 
-MYSQL_PASSWORD=${MYSQL_PASSWORD:-"admin"} 
+MYSQL_PASSWORD=${MYSQL_PASSWORD:-"root"} 
 
 
 MYSQL_USER="root"
