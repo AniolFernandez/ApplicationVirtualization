6a8eb504da019ba5ccdfbcf0165b5a1c57e639f2
AniolFernandez <aniol.fdz@gmail.com>
2023-05-19 21:06:19 +0200

    feat: ini db

---
+++ install/inidb.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo apt-get install mysql-client-core-8.0 -y > /dev/null
+
+read -p "Escriu el host on es situa la BDD: [Per defecte: 127.0.0.1]: " MYSQL_HOST 
+MYSQL_HOST=${MYSQL_HOST:-"127.0.0.1"} 
+read -p "Escriu la cotrasenya de root [Per defecte: root]: " MYSQL_PASSWORD 
+MYSQL_PASSWORD=${MYSQL_PASSWORD:-"admin"} 
+
+
+MYSQL_USER="root"
+
+wget https://raw.githubusercontent.com/AniolFernandez/ApplicationVirtualization/main/install/ini.sql -O ini.sql
+
+mysql -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASSWORD < ini.sql && echo "BDD inicialitzada."
+rm ini.sql
